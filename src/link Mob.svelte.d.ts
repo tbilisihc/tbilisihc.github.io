2a8419553b56591597f4365d415560455e199bca
drew <realandrinoff@gmail.com>
2025-06-14 16:36:07 +0400

    Removed d.ts files

--- src/link Mob.svelte.d.ts
+++
@@ -1,6 +0,0 @@
-export default LinkMob;
-type LinkMob = {
-    $on?(type: string, callback: (e: any) => void): () => void;
-    $set?(props: $$ComponentProps): void;
-};
-declare const LinkMob: import("svelte").Component<$$ComponentProps, {}, "">;
--- src/link.svelte.d.ts
+++
@@ -1,6 +0,0 @@
-export default Link;
-type Link = {
-    $on?(type: string, callback: (e: any) => void): () => void;
-    $set?(props: $$ComponentProps): void;
-};
-declare const Link: import("svelte").Component<$$ComponentProps, {}, "">;
