2d0b317cd66342475f07575e6a8e8e5e305ead69
drew <realandrinoff@gmail.com>
2025-07-03 22:47:28 +0400

    fix(meta): titles
    added appropriate titles to every page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <svelte:head>
+    <title>Home | Tbilisi Hack Club</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
