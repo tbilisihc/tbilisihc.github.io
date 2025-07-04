7b86c44ee2ed4bb79ecec75f7ce2868d00fa61a9
drew <realandrinoff@gmail.com>
2025-07-04 18:59:17 +0400

    mock change

---
+++ src/lib/arrayss/team.js
@@ -0,0 +1,19 @@
+let team = [
+  {
+    Name: "Drew",
+    Avatar: "https://tbilisi.hackclub.com/drew/avatar.png",
+    Url: "https://tbilisi.hackclub.com/drew/",
+  },
+  {
+    Name: "Boris Mirakov",
+    Avatar: "https://tbilisi.hackclub.com/bormir/avatar.png",
+    Url: "https://tbilisi.hackclub.com/bormir/",
+  },
+  {
+    Name: "Zlata Chernova",
+    Avatar: "https://tbilisi.hackclub.com/zlataponchikk/avatar.png",
+    Url: "https://tbilisi.hackclub.com/zlataponchikk",
+  },
+];
+
+export default team
