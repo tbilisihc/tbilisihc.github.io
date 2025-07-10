217b7ad7c1fa586b4fb685102b1dc17552cba22f
Drew <realandrinoff@gmail.com>
2025-07-10 20:06:23 +0400

    fix: tbilisihc/tbilisihc.github.io#39
    Added responsive layout change <= 1020px in width

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,59 +1,69 @@
 <script>
-    import { _ } from "svelte-i18n";
-    import navigation from "$lib/arrays/navigation.js";
+  import { _ } from "svelte-i18n";
+  import navigation from "$lib/arrays/navigation.js";
 </script>
 
 <footer class="bg-gray-800 text-white py-8">
-    <div class="container mx-auto px-6">
-        <div class="flex flex-wrap">
-            <div class="mt-20 w-full md:w-1/2">
-                <h3 class="text-lg font-bold">{$_("navigation.about")}</h3>
-                <p class="mt-2 max-w-md">&nbsp; {$_(`footer.about`)}</p>
-            </div>
-            <div class="mt-20 w-full md:w-1/4">
-                <h3 class="text-lg font-bold">{$_("navigation.links")}</h3>
-                <ul class="mt-2 space-y-2">
-                    {#each navigation.about as item}
-                        <li>
-                            <a href={item.href} class="hover:underline">
-                                {$_(`${item.name}`)}
-                            </a>
-                        </li>
-                    {/each}
-                </ul>
-            </div>
-            <div class="mt-20 w-full md:w-1/4">
-                <h3 class="text-lg font-bold">Social</h3>
-                <ul class="mt-2 space-y-2">
-                    {#each navigation.social as item}
-                        <li>
-                            <a
-                                href={item.href}
-                                class="hover:underline"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                {item.name}
-                            </a>
-                        </li>
-                    {/each}
-                </ul>
-            </div>
-        </div>
-        <div class="mt-8 border-t border-gray-700 pt-4 text-center">
-            <p>
-                &copy; {new Date().getFullYear()} Tbilisi Hack Club. {$_(
-                    "footer.right",
-                )}
-            </p>
-        </div>
+  <div class="container mx-auto px-6">
+    <div class="columns flex flex-wrap">
+      <div class="mt-20 w-full md:w-1/2">
+        <h3 class="text-lg font-bold">{$_("navigation.about")}</h3>
+        <p class="mt-2 max-w-md">&nbsp; {$_(`footer.about`)}</p>
+      </div>
+      <div class="mt-20 w-full md:w-1/4">
+        <h3 class="text-lg font-bold">{$_("navigation.links")}</h3>
+        <ul class="mt-2 space-y-2">
+          {#each navigation.about as item}
+            <li>
+              <a href={item.href} class="hover:underline">
+                {$_(`${item.name}`)}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+      <div class="mt-20 w-full md:w-1/4">
+        <h3 class="text-lg font-bold">Social</h3>
+        <ul class="mt-2 space-y-2">
+          {#each navigation.social as item}
+            <li>
+              <a
+                href={item.href}
+                class="hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.name}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
     </div>
+    <div class="mt-8 border-t border-gray-700 pt-4 text-center">
+      <p>
+        &copy; {new Date().getFullYear()} Tbilisi Hack Club. {$_(
+          "footer.right"
+        )}
+      </p>
+    </div>
+  </div>
 </footer>
 
 <style>
-    @media (max-width: 768px) {
-        .container {
-            margin: 20px;
-        }
+  footer {
+    background-color: #1f2937; /* Gray 800 */
+    color: rgba(255, 255, 255, 0.702);
+  }
+  h3 {
+    color: #f3f4f6; /* Gray 200 */
+  }
+  @media (max-width: 1020px) {
+    .columns {
+      text-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+      flex-direction: column;
     }
+  }
 </style>
